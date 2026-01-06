62d370fad73c59c8675439658d23bc6907325245
sampath Kumar <sampathk@x.com>
2026-01-05 22:13:43 -0800

    Add exercises directory for Rust learning chapters

---
+++ exercises/hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a.d
@@ -0,0 +1,5 @@
+/Users/sampathk/learning/rust-learning-journey/exercises/hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a.d: src/main.rs
+
+/Users/sampathk/learning/rust-learning-journey/exercises/hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a: src/main.rs
+
+src/main.rs:
---
+++ exercises/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/sampathk/learning/rust-learning-journey/exercises/hello_cargo/target/debug/hello_cargo: /Users/sampathk/learning/rust-learning-journey/exercises/hello_cargo/src/main.rs
